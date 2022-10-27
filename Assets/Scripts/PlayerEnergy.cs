a7a2d1495f1228c6b4f7888abc4b813aa459af77
Nikita <shpulinn@mail.ru>
2022-10-27 11:25:04 +0200

    PlayerEnergy script created

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEnergy : MonoBehaviour
+{
+    [SerializeField] private Slider energySlider;
+
+    private float _currentEnergy;
+
+    private void Start()
+    {
+        _currentEnergy = energySlider.value;
+        UpdateEnergySlider();
+    }
+
+    public void IncreaseEnergy(float amount)
+    {
+        _currentEnergy += amount;
+        UpdateEnergySlider();
+    }
+
+    public void DecreaseEnergy(float amount)
+    {
+        _currentEnergy -= amount;
+        UpdateEnergySlider();
+    }
+
+    private void UpdateEnergySlider()
+    {
+        energySlider.value = _currentEnergy;
+    }
+}
