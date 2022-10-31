23f4b0b9510fb94c7be707db82f0029f7e532782
Nikita <shpulinn@mail.ru>
2022-11-01 00:23:35 +0200

    Update UIController.cs

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,11 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    public static UIController Instance;
+    
+    [SerializeField] private GameObject gameOverDeathScreen;
+    [SerializeField] private GameObject gameOverWinScreen;
+    [SerializeField] private Text goalAmountText;
+    [SerializeField] private Text currentAmountText;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+
     void Update()
     {
         if (EventSystem.current.IsPointerOverGameObject())
@@ -18,5 +34,35 @@ public class UIController : MonoBehaviour
         {
             InputManager.Instance.ToggleControl(false);
         }
+    }
+
+    public void ShowDeathScreen()
+    {
+        gameOverDeathScreen.SetActive(true);
+    }
+
+    public void ShowWinScreen()
+    {
+        gameOverWinScreen.SetActive(true);
+    }
+
+    public void SetGoalAmount(int amount)
+    {
+        goalAmountText.text = amount.ToString();
+    }
+
+    public void UpdateCurrentScore(int amount)
+    {
+        currentAmountText.text = amount.ToString();
+    }
+
+    public void PlayAgainButtonClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitButtonClick()
+    {
+        Application.Quit();
     }
 }
