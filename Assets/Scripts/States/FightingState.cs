52e49176ac06ff7ee56633b08a6e70fc6f2fcd38
Nikita <shpulinn@mail.ru>
2022-11-01 12:18:10 +0200

    Update FightingState.cs
    changed way to get click (tap) point on smartphone

--- Assets/Scripts/States/FightingState.cs
+++ Assets/Scripts/States/FightingState.cs
@@ -11,6 +11,23 @@ public class FightingState : BaseState
 
     public override void Transition()
     {
+        if (InputManager.Instance.Tap)
+        {
+            Ray ray;
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                ray = Camera.main.ScreenPointToRay(InputManager.Instance.MousePosition);
+            } else 
+                ray = Camera.main.ScreenPointToRay(UnityEngine.InputSystem.Touchscreen.current.touches[0].position.ReadValue());
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Move player to click position
+                GetComponent<RunningState>().MoveToPoint(hit.point);
+                playerMotor.ChangeState(GetComponent<RunningState>());
+            }
+        }
         // other transitions here:
         // Idle state
         // Running state
