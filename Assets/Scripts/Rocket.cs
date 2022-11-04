37dc1918f0df0e3331e44677d3c547e74b4b8076
Nikita <shpulinn@mail.ru>
2022-11-04 13:09:17 +0200

    Update Rocket.cs
    added damage logic

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] private float damageRadius = 5.0f;
+    [SerializeField] private float damage = 10.0f;
     [SerializeField] private GameObject explosionPrefab;
-
+    
     private void OnTriggerEnter(Collider other)
     {
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        MakeDamage();
         Destroy(gameObject);
+    }
+
+    private void MakeDamage()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, damageRadius);
+        foreach (var col in colliders)
+        {
+            if (col.TryGetComponent(out EnemyHealth enemyHealth))
+            {
+                enemyHealth.TakeDamage(damage);
+            } else continue;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(100, 100, 100, .3f);
+        Gizmos.DrawSphere(transform.position, damageRadius);
     }
 }
