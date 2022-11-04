71728742d774c89e1cfecfebe43e136d9801194f
Nikita <shpulinn@mail.ru>
2022-11-04 11:09:42 +0200

    Update RocketMissileState.cs
    added rocket missile logic

--- Assets/Scripts/States/RocketMissileState.cs
+++ Assets/Scripts/States/RocketMissileState.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RocketMissileState : BaseState
 {
+    [SerializeField] private GameObject rocketPrefab;
+    
     public override void Construct()
     {
         stateName = "Rocket missile";
@@ -15,6 +17,23 @@ public class RocketMissileState : BaseState
 
     public override void Transition()
     {
+        if (InputManager.Instance.Tap)
+        {
+            Ray ray;
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                ray = Camera.main.ScreenPointToRay(InputManager.Instance.MousePosition);
+            } else 
+                ray = Camera.main.ScreenPointToRay(UnityEngine.InputSystem.Touchscreen.current.touches[0].position.ReadValue());
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Instantiate rocket above this point 
+                Quaternion rot = new Quaternion(0, 0, 180, -1);
+                Instantiate(rocketPrefab, hit.point + Vector3.up * 10, rot);
+            }
+        }
         // other transitions here:
         // Idle state
         // Running state
