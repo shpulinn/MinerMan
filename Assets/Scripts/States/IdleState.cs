05f2046e362263794393d70dc902d075c42b5bcc
Nikita <shpulinn@mail.ru>
2022-11-05 15:10:08 +0200

    Update IdleState.cs
    perfomance improvements tries

--- Assets/Scripts/States/IdleState.cs
+++ Assets/Scripts/States/IdleState.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IdleState : BaseState
 {
+    private RunningState _runningState;
+    private MiningState _miningState;
+    private FightingState _fightingState;
+    private RocketMissileState _rocketMissileState;
+    private DeathState _deathState;
+
+    private void Start()
+    {
+        _runningState = GetComponent<RunningState>();
+        _miningState = GetComponent<MiningState>();
+        _fightingState = GetComponent<FightingState>();
+        _rocketMissileState = GetComponent<RocketMissileState>();
+        _deathState = GetComponent<DeathState>();
+    }
+
     public override void Construct()
     {
         stateName = "Idle";
@@ -25,31 +41,37 @@ public class IdleState : BaseState
             if (Physics.Raycast(ray, out hit))
             {
                 // Move player to click position
-                //_motor.MoveToPoint(hit.point);
-                GetComponent<RunningState>().MoveToPoint(hit.point);
-                playerMotor.ChangeState(GetComponent<RunningState>());
+                
+                //GetComponent<RunningState>().MoveToPoint(hit.point);
+                //playerMotor.ChangeState(GetComponent<RunningState>());
+                
+                _runningState.MoveToPoint(hit.point);
+                playerMotor.ChangeState(_runningState);
             }
         }
 
         if (playerMotor.CanMiningEnergy || playerMotor.CanMiningCrystal)
         {
-            playerMotor.ChangeState(GetComponent<MiningState>());
+            //playerMotor.ChangeState(GetComponent<MiningState>());
+            playerMotor.ChangeState(_miningState);
         }
 
         if (playerMotor.IsFighting)
         {
-            playerMotor.ChangeState(GetComponent<FightingState>());
+            //playerMotor.ChangeState(GetComponent<FightingState>());
+            playerMotor.ChangeState(_fightingState);
         }
 
+        if (playerMotor.IsRocketing)
+        {
+            //playerMotor.ChangeState(GetComponent<RocketMissileState>());
+            playerMotor.ChangeState(_rocketMissileState);
+        }
+        
         if (playerMotor.IsDead)
         {
-            playerMotor.ChangeState(GetComponent<DeathState>());
+            //playerMotor.ChangeState(GetComponent<DeathState>());
+            playerMotor.ChangeState(_deathState);
         }
-
-        // other transitions here:
-        // Mining state
-        // Fighting state
-        // RocketMissile state 
-        // Death state
     }
 }
