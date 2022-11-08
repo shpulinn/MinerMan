528bbae3e13eaeb292b6788166b42cc089de3b86
Nikita <shpulinn@mail.ru>
2022-11-08 14:06:09 +0200

    Update EnemyMovement.cs
    added 2 types of enemies: guarding anf patrolling;
    added behaviour style for each enemy type;
    added moving enemies for each type;.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,14 +8,30 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     // radius of range when enemy would "see" player and start follow
-    [SerializeField] private float visionRadius = 10.0f;
+    [SerializeField] private float visionRadius = 7.0f;
     [SerializeField] private float moveSpeed = 2.0f;
     [SerializeField] private float rotationSpeed = 100.0f;
+    // the distance between enemy & player to stop following and back to guarding/patrolling
+    [SerializeField] private float stopFollowingDistance = 7.0f; 
+    [SerializeField] private List<Transform> wayPoints = new List<Transform>();
+    private int _currentWayPointIndex;
+
+    public enum EnemyType
+    {
+        Guarding, // just guarding it's position
+        Patrolling // patrol the area via waypoints (wayPoints list)
+    }
+
+    public EnemyType enemyType;
 
     private NavMeshAgent _meshAgent;
     private Vector3 _startPosition;
     private GameObject _player;
     private SphereCollider _sphereCollider;
+
+    private bool _isChasing = false;
+    private bool _isGuarding = false;
+    private bool _isPatrolling = false;
 
     private void Start()
     {
@@ -27,6 +43,38 @@ public class EnemyMovement : MonoBehaviour
         _sphereCollider = GetComponent<SphereCollider>();
         _sphereCollider.radius = visionRadius;
         _sphereCollider.isTrigger = true;
+        _currentWayPointIndex = 0;
+        
+        switch (enemyType)
+        {
+            case EnemyType.Guarding:
+                StartGuarding();
+                break;
+            case EnemyType.Patrolling:
+                StartPatrol();
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    private void CheckDistance()
+    {
+        if (_isPatrolling)
+        {
+            if (Vector3.Distance(transform.position, wayPoints[_currentWayPointIndex].position) <= _meshAgent.stoppingDistance)
+            {
+                _currentWayPointIndex++;
+                if (_currentWayPointIndex >= wayPoints.Count)
+                {
+                    _currentWayPointIndex = 0;
+                }
+                _meshAgent.SetDestination(wayPoints[_currentWayPointIndex].position);
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -40,6 +88,48 @@ public class EnemyMovement : MonoBehaviour
 
     private void StartChasingPlayer()
     {
+        _isGuarding = false;
+        _isPatrolling = false;
         _meshAgent.SetDestination(_player.transform.position);
+        if (Vector3.Distance(transform.position, _player.transform.position) >= stopFollowingDistance
+            && _isChasing)
+        {
+            StopChasingPlayer();
+        }
+
+        _isChasing = true;
+    }
+
+    private void StartPatrol()
+    {
+        _isPatrolling = true;
+        if (Vector3.Distance(transform.position, wayPoints[_currentWayPointIndex].position) > _meshAgent.stoppingDistance)
+        {
+            _meshAgent.SetDestination(wayPoints[_currentWayPointIndex].position);
+        }
+    }
+
+    private void StartGuarding()
+    {
+        _isPatrolling = true;
+        if (Vector3.Distance(transform.position, _startPosition) > _meshAgent.stoppingDistance)
+        {
+            _meshAgent.SetDestination(_startPosition);
+        }
+    }
+
+    private void StopChasingPlayer()
+    {
+        _isChasing = false;
+        switch (enemyType)
+        {
+            case EnemyType.Guarding:
+                StartGuarding();
+                break;
+            case EnemyType.Patrolling:
+                StartPatrol();
+                break;
+        }
+        
     }
 }
