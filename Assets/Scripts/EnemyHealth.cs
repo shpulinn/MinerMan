ddc2ddd2e02e40461a070ea88c85c3daa3dcd9c4
Nikita <shpulinn@mail.ru>
2022-11-10 18:36:53 +0200

    Update EnemyHealth.cs
    added animations for taking damage and death

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,21 +1,37 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealthPoints = 10.0f;
+    [SerializeField] private ParticleSystem bloodParticles;
+    [SerializeField] private float timeToDestroyAfterDeath = 2.0f;
 
     private float _currentHP;
+    private Animator _animator;
+    private EnemyMovement _enemyMovement;
+
+    private int takeDamageAnimationTriggerID;
+    private int isDeadAnimationID;
 
     private void Start()
     {
         _currentHP = maxHealthPoints;
+        _animator = GetComponent<Animator>();
+        _enemyMovement = GetComponent<EnemyMovement>();
+
+        AssignAnimationsID();
+    }
+
+    private void AssignAnimationsID()
+    {
+        takeDamageAnimationTriggerID = Animator.StringToHash("TakeDamage");
+        isDeadAnimationID = Animator.StringToHash("IsDead");
     }
 
     public void TakeDamage(float damage)
     {
+        bloodParticles.Play();
+        _animator.SetTrigger(takeDamageAnimationTriggerID);
         _currentHP -= damage;
         if (_currentHP <= 0)
         {
@@ -25,6 +41,13 @@ public class EnemyHealth : MonoBehaviour
 
     private void Die()
     {
-        Destroy(gameObject);
+        _enemyMovement.DeathAction();
+        _animator.SetBool(isDeadAnimationID, true);
+        Destroy(gameObject, timeToDestroyAfterDeath);
+        
+        // Destroying this component and EnemyMovement component for "disabling" enemy, 
+        // but leave on scene for "timeToDestroyAfterDeath" seconds after death and then disappear
+        Destroy(this);
+        Destroy(_enemyMovement);
     }
 }
