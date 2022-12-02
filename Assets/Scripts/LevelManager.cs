3652128b7d601e4f1eda6c77e985198c6c82af72
Nikita <shpulinn@mail.ru>
2022-12-02 12:41:01 +0200

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,11 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LevelType
+{
+    Crystal,
+    Boss
+}
+
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
 
     [SerializeField] private int levelCrystalGoal = 5;
+    [SerializeField] private GameObject crystalImage;
+    [SerializeField] private GameObject bossLabel;
+
+    [SerializeField] private LevelType _levelType;
 
     public int LevelCrystalGoal => levelCrystalGoal;
     
@@ -25,8 +35,26 @@ public class LevelManager : MonoBehaviour
         }
 
         Instance = this;
-        
-        UIController.Instance.SetGoalAmount(levelCrystalGoal);
+
+        switch (_levelType)
+        {
+            case LevelType.Crystal:
+                UIController.Instance.SetGoalAmount(levelCrystalGoal);
+                if (bossLabel)
+                {
+                    bossLabel.SetActive(false);
+                }
+                crystalImage.SetActive(true);
+            break;
+            case LevelType.Boss:
+                UIController.Instance.SetGoalAmount(1); // 1 boss
+                if (crystalImage)
+                {
+                    crystalImage.SetActive(false);
+                }
+                bossLabel.SetActive(true);
+                break;
+        }
     }
 
     public void FinishLevel(FinishTypes type)
