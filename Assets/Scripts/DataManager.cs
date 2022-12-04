8095fe9262167158d5d9a5696c7012a99b759f3a
Nikita <shpulinn@mail.ru>
2022-12-04 15:16:36 +0200

    DataManager script created

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+}
